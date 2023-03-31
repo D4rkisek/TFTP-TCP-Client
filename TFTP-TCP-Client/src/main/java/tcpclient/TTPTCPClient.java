1f2d5b3443d0579907e4edd80c372b9f74838ced
mf479 <maruf.f@hotmail.com>
2023-03-31 14:11:29 +0100

    .

--- TFTP-TCP-Client/src/main/java/tcpclient/TTPTCPClient.java
+++ TFTP-TCP-Client/src/main/java/tcpclient/TTPTCPClient.java
@@ -5,14 +5,16 @@ import java.net.*;
 
 public class TTPTCPClient {
 
-    private static final String DEFAULT_SERVER_ADDRESS = "127.0.0.1";
+    private static final String DEFAULT_SERVER_ADDRESS = "127.0.0.1"; // Local host
     private static final int DEFAULT_SERVER_PORT = 9222;
     private static final int BUFFER_SIZE = 512;
 
     public static void main(String[] args) {
+        // Putting Default values
         String serverAddress = DEFAULT_SERVER_ADDRESS;
         int serverPort = DEFAULT_SERVER_PORT;
 
+        // Initialisation of socket, and stream objects
         try (Socket socket = new Socket(serverAddress, serverPort);
              DataInputStream in = new DataInputStream(socket.getInputStream());
              DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
@@ -27,7 +29,7 @@ public class TTPTCPClient {
                     break;
                 }
                 System.out.print("Enter the filename: ");
-                String filename = stdIn.readLine().trim();
+                String filename = stdIn.readLine().trim(); // Getting rid of spaces and other additional keys
 
                 if ("1".equals(command)) {
                     sendRequest(out, "WRQ", filename);
@@ -45,12 +47,18 @@ public class TTPTCPClient {
         }
     }
 
+
     private static void sendRequest(DataOutputStream out, String requestType, String filename) throws IOException {
+        // Concatenation of the request type, filename,
+        // and "octet" (the mode). The null bytes ('\0') are required by the TFTP protocol
         String request = requestType + "\0" + filename + "\0octet\0";
+        // Writes the request string to the output stream as a byte array.
         out.write(request.getBytes());
     }
 
+
     private static void receiveFile(DataInputStream in, String filename) throws IOException {
+        // Creating a new file with the specified filename.
         File file = new File(filename);
         if (file.exists()) {
             file.delete();
@@ -60,8 +68,12 @@ public class TTPTCPClient {
             byte[] buffer = new byte[BUFFER_SIZE];
             int bytesRead;
 
+            // Read data from the input stream into the buffer until there is no more data,
+            // writing the contents of the buffer to the file as we go
             while ((bytesRead = in.read(buffer)) != -1) {
                 fos.write(buffer, 0, bytesRead);
+                // If we read less than BUFFER_SIZE i.e., 512 bytes, we know we have reached the end of
+                // the stream and can break out of the loop
                 if (bytesRead < BUFFER_SIZE) {
                     break;
                 }
@@ -75,11 +87,12 @@ public class TTPTCPClient {
             System.out.println("File not found");
             return;
         }
-
+        // Open a new FileInputStream to read the contents of the file
         try (FileInputStream fis = new FileInputStream(file)) {
             byte[] buffer = new byte[BUFFER_SIZE];
             int bytesRead;
 
+            // Writes the content from the out para, reading in bytes
             while ((bytesRead = fis.read(buffer)) != -1) {
                 out.write(buffer, 0, bytesRead);
             }
